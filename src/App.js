20f4b0001e16df4399533d1710f7077a75f17fdf
Krishnat Mahajan <mahajankrishna798@gmail.com>
2024-02-07 20:07:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { increase , decrease } from './actions'
 function App() {
+  const myState = useSelector((state) => state.inc_dec);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{font:"bold"}}>Welcome to React Redux Application</h1>
+      <input type='text' value={myState}/>
+      <br />
+      <br />
+      <button onClick={() => dispatch(increase())} className='btn btn-primary'>Increment</button>
+      <button onClick={() => dispatch(decrease())} className='btn btn-danger'>Decrement</button>
     </div>
   );
 }
 
 export default App;
+
